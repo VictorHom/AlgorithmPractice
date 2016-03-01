a40e0cd94bb91a98a66a7df429da73f5c3ea7735
Victor Hom <vh62@cornell.edu>
2016-03-01 10:07:12 -0500

    started mergesort

---
+++ Sorts/merge.js
@@ -0,0 +1,15 @@
+
+const splitList = (list) => {
+	if (list.length <= 1) {
+		return list;
+	} else {
+		let splitPoint = Math.floor(list.length / 2.0);
+		// you could use some ... spread operator for split the arr into two
+	}
+}
+
+const mergeSort = (list) => {
+
+}
+
+// merge sort is made up of a merge piece and a sort piece
