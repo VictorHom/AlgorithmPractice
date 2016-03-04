740ea80460a11fe648499b6eed6845ae2f3a6532
Victor Hom <vh62@cornell.edu>
2016-03-03 21:59:28 -0500

    wrote mergesort finally, need to comment and clean up

--- Sorts/merge.js
+++ Sorts/merge.js
@@ -6,11 +6,39 @@ const split = (list) => {
 }
 
 const merge = (listA, listB) => {
+	// do merge sort in here if it doesn't work
+	if (listA.length <= 1 || listB.length <= 1) {
+		if (listA.length === 1 && listB.length === 1) {
+			if (listA[0] > listB[0]) {
+				return listB.concat(listA);
+			} else {
+				return listA.concat(listB);
+			}
+		} else {
+			// order does not matter because one is empty
+			return listA.concat(listB);
+		}
+	} else {
+		let result = []
+		if (listA.length >= 1) {
+			result = result.concat(mergeSort(listA));
+		}
+		if (listB.length >= 1) {
+			result = result.concat(mergeSort(listB));
+		}
+		return result;
+	}
+	// else {
+	// 	return 
+	// }
+
 }
 
-const mergeSort = (list) => {
-	let splitUp = split(list);
-	merge(splitUp[0], splitUp[1]);
+export const mergeSort = (list) => {
+	let splitUp = split(list); 
+	var final = merge(splitUp[0], splitUp[1]);
+	console.log(final);
+	return final;
 }
 //split to smallest parts
 //combine 
--- SortsTests/sort.test.js
+++ SortsTests/sort.test.js
@@ -2,6 +2,8 @@ let expect = require('chai').expect;
 import { insertionSort } from '../Sorts/insertion';
 import { selectionSort } from '../Sorts/selection';
 import { bubbleSort } from '../Sorts/bubble';
+import { mergeSort } from '../Sorts/merge';
+//export default
 
 
 var cases = [
@@ -11,7 +13,8 @@ var cases = [
 	[9, 3],
 	[1, 2, 3],
 	[1, 4, 6, 3],
-	[6, 4, 3, 2, 1]
+	[6, 4, 3, 2, 1],
+	[5, 6, 1, 34, 2, 3, 7,8,56, 90]
 ];
 
 let cases_answers = [
@@ -21,7 +24,8 @@ let cases_answers = [
 	[3, 9],
 	[1, 2, 3],
 	[1, 3, 4, 6],
-	[1, 2, 3, 4, 6]
+	[1, 2, 3, 4, 6], 
+	[1, 2, 3, 5, 6, 7, 8, 34, 56, 90]
 ];
 
 /** initial test to make sure mocha and chai work */
@@ -40,6 +44,7 @@ const arrayTests = (cbFunction) =>{
 	expect(cbFunction(cases[4])).to.deep.equal(cases_answers[4]);
 	expect(cbFunction(cases[5])).to.deep.equal(cases_answers[5]);
 	expect(cbFunction(cases[6])).to.deep.equal(cases_answers[6]);
+	expect(cbFunction(cases[7])).to.deep.equal(cases_answers[7]);
 }
 
 describe('this is testing insertion sort', () => {
@@ -58,5 +63,12 @@ describe('this is testing selection sort', () => {
 describe('this is testing bubble sort', () => {
 	it('should order the test cases', () => {
 		arrayTests(bubbleSort);
+	});
+});
+
+
+describe('this is testing merge sort', () => {
+	it('should order the test cases', () => {
+		arrayTests(mergeSort);
 	});
 });
