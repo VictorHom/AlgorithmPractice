3d2369b3fe6ca45aa3c4564d4592198e33353b55
Victor Hom <vh62@cornell.edu>
2016-03-08 18:21:22 -0500

    added some notes on the string permutation

---
+++ General/PalindromePermutation.js
@@ -0,0 +1,14 @@
+//*Permutations of a Palindrome*
+//Given a string, is it a permutation of a palindrome?
+isPalPerm('coactat') //true  ('tacocat')
+
+//*Approach*
+//Let's avoid a brute force search. That would work but get infeasible rather quickly as the length of our string increases
+//Instead, guide your interviewee to think about what makes a word a palindrome:
+    //The same forwards and backwards
+    //So, the first half of the string is repeated for the second half, just in reverse
+    //'hannah' -> 'han' + 'nah'
+    //This means that there is an even number of every letter.
+    //Except maybe one: 'racecar' -> 2r, 2a, 2c, 1e
+    //This means we can only ever have one character count that is odd, the rest must be even
+//See if you can get your interviewee to this approach by telling them we want an O(n) solution
--- General/stringPermutation.js
+++ General/stringPermutation.js
@@ -31,3 +31,41 @@ export const stringPermutation = (str) => {
 
 // It's pretty slow right now
 // does it mean you want to have the shorter snippets or just the full length ones
+//below is much quicker
+// n!
+
+// the non es6 solution
+// function stringPermutation(str) {
+// 	var differentPerms = [' ']; //important to have a space so that you have length
+// 	var index = 0;
+// 	while(index < str.length){
+// 		var currentPerms = [];
+// 		for (var i = 0; i < differentPerms.length; i++) {
+// 			var currentChar = str[index];
+// 			for (var j = 0; j < differentPerms[i].length; j++) {
+// 			    var currentPerm = differentPerms[i].slice(0, j) + currentChar + differentPerms[i].slice(j);
+// 			    currentPerms.push(currentPerm);
+// 			}
+// 		}
+// 		differentPerms = differentPerms.concat(currentPerms);
+// 		index = index + 1;
+// 	}
+// 	return differentPerms;
+// }
+
+// Another solution- not mine, but definitely clearer for a recursive solution
+// function permutations(str) {
+//     if(str.length === 1) return [str];
+//     var perms = [];
+//     for(var i = 0; i < str.length; i++) {
+//         var char = str[i];
+//         permutations(str.slice(0,i) + str.slice(i+1)).forEach(function(perm) {
+//             perms.push(char + perm);
+//         });
+//     }
+//     var sorted = perms.sort();
+//     return sorted.filter(function(perm, i) {
+//         return perm !== sorted[i+1];
+//     });
+// }
+
