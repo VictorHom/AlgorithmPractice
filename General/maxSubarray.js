606e6f32e7c3e1848c2d6498d1267cdc38bfd67a
Victor Hom <vh62@cornell.edu>
2016-03-14 23:12:45 -0400

    need to check on maxSubarray

--- General/maxSubarray.js
+++ General/maxSubarray.js
@@ -1,14 +1,23 @@
 const maxSubarray = (arr) => {
-	let minSum = 0, maxSum = 0, lb =0, ub = 0;
+	let minSum = 0, maxSum = 0, minlb = 0, minub = 0, maxlb = 0, maxub = 0;
 
 	for (let i = 0; i < arr.length; i++) {
 		let currentVal = arr[i];
 		// if the current val does not add value
 		if (currentVal + maxSum < maxSum) {
-
+			// do I need to check if the current maxSum
+			if (maxSum > minSum) {			
+				minSum = maxSum;
+				minlb = maxlb;
+				minub = maxub;
+			}
 		//if the current val does add value
 		} else if (currentVal + maxSum > maxSum) {
-
-		}
+			maxub = maxub + 1;
+		}	
 	}
+
+	if (maxSum > minSum) return arr.slice(maxlb, maxub);
+	if (maxSum < minSum) return arr.slice(minlb, minub);
 }
+// need to test;
