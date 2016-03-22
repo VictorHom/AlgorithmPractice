affff23ab95441289086aa7c5595585526fa710b
Victor Hom <vh62@cornell.edu>
2016-03-22 10:00:35 -0400

    off by 1 in the rain collector, need debug

--- General/rainBucketCollection.js
+++ General/rainBucketCollection.js
@@ -1,24 +1,41 @@
+
 const rainCollector = (arr) => {
-  //
-  let highestPoint = arr[0];
-  let highestPointIndex = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > highestPoint) {
-      highestPoint = arr[i];
-      highestPointIndex = i;
+  let total = 0 ;
+  let highPoint = arr[0];
+  let highPointIndex = 0;
+
+  arr.forEach( (elem, index) =>{
+    if (elem > highPoint) {
+      highPoint = elem;
+      highPointIndex = index;
+    }
+  });
+//   console.log(highPoint);
+//   console.log(highPointIndex);
+
+  for (let i = 0; i >= 0; i--) {
+    let compareLowerIndex = highPointIndex;
+    let compareUpperIndex = highPointIndex;
+
+    for (let j = highPoint; j >= 0; j--) {
+        console.log("current j:", j )
+      if (arr[j] === i && compareLowerIndex !== j) {
+          console.log("Lowerindex:", compareLowerIndex)
+        total = total + (compareLowerIndex - j - 1);
+        compareLowerIndex = j;
+      }
+    }
+
+    for (let j = highPointIndex; j < arr.length; j++) {
+        console.log("upper index:", compareUpperIndex)
+      if (arr[j] > i && compareUpperIndex !== j) {
+        total = total + (j - compareUpperIndex - 1)
+        compareUpperIndex = j;
+      }
     }
   }
-  // should have the highest point and index;
-  for (let j = highestPoint; j >= 0; j--) {
-    // everything before the peak
-    for (let i = highestPointIndex; i >= 0 ; i--) {
-      
 
-    }
-    // everything past the peak
-    for (let i = arr.length; i >= highestPointIndex; i--) {
-
-    }
-  }
-
+  return total;
 }
+rainCollector([0,1,0,2,1,0,1,3,2,1,2,1]);
+// rainCollector([2,1,2])
