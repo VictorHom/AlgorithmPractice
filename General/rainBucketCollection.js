def3149546a15dbf09ae4eca4f7c5804dfcb330e
Victor Hom <vh62@cornell.edu>
2016-03-21 21:57:27 -0400

    created basic structure for the rain bucket question

---
+++ General/rainBucketCollection.js
@@ -0,0 +1,16 @@
+const rainCollector = (arr) => {
+  //
+  let highestPoint = arr[0];
+  let highestPointIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > highestPoint) {
+      highestPoint = arr[i];
+      highestPointIndex = i;
+    }
+  }
+  // should have the highest point and index;
+  for (let j = highestPoint; i=j >= 0; j--) {
+    
+  }
+
+}
