a6988d891db62936e5d9173e0ed5211137b16d25
Victor Hom <vh62@cornell.edu>
2016-04-07 19:23:27 -0400

    part of intersect love rectangles

---
+++ General/intersectLoveRectangles.js
@@ -0,0 +1,30 @@
+// rectanle format
+// var myRectangle =  {leftX: 1, bottomY : 5, width : 10, height : 4}
+// pass in a rectangle and the other rectangle's points
+const containsAtLeastOnePoint = (rect, overLapperPoints) => {
+  for (let i = 0; i < overLapperPoints.length; i++) {
+
+  }
+  return false;
+}
+
+
+const intersectLoveRectangles = (rect1, rect2) => {
+  // order rectangles for my sanity
+  let {leftRect, rightRect} = (rect1.leftX < rect2.leftX) ? { rect1, rect2 } : { rect2, rect1 };
+
+  // this is the left rectangle points
+  let leftRectBottomLeft = [ leftRect.leftX, leftRect.bottomY ];
+  let leftRectTopLeft = [ leftRect.leftX, leftRect.bottomY + leftRect.height];
+  let leftRectBottomRight = [ leftRect.leftX+leftRect.width, leftRect.bottomY ];
+  let leftRectTopRight = [ leftRect.leftX+leftRect.width, leftRect.bottomY + leftRect.height ];
+
+  // this is the right rectanle points
+  let rightRectBottomLeft = [ rightRect.leftX, rightRect.bottomY ];
+  let rightRectTopLeft = [ rightRect.leftX, rightRect.bottomY + rightRect.height];
+  let rightRectBottomRight = [ rightRect.leftX+rightRect.width, rightRect.bottomY ];
+  let rightRectTopRight = [ rightRect.leftX+rightRect.width, rightRect.bottomY + rightRect.height ];
+
+  let leftRectContainsRightRect = containsAtLeastOnePoint();
+  let rightRectContainsLeftRect = containsAtLeastOnePoint();
+}
