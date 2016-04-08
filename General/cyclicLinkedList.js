7298cdad82e8aa2a7c7369cc34ea70c7f21302de
Victor Hom <vh62@cornell.edu>
2016-04-07 23:03:58 -0400

    first test working for finding information about cyclic Linked list

--- General/cyclicLinkedList.js
+++ General/cyclicLinkedList.js
@@ -50,7 +50,7 @@ const isThereCycle = (node) => {
   while (tracker2.next !== null) {
     if (tracker1 === tracker2 && start !== 0) {
       // there is cycle because tracker2 will have caught up with tracker1
-      return true;
+      return tracker1;
     }
     // going by 1
     tracker1 = tracker1.next;
@@ -67,6 +67,35 @@ const isThereCycle = (node) => {
   return false;
 }
 
+const findCycleLength = (node) => {
+  let sizeCount = 0;
+  let anchorNode = node;
+  let runner = node.next;
+  while (anchorNode !== runner) {
+    sizeCount += 1;
+    runner = runner.next;
+  }
+  return sizeCount;
+};
+
+// need to check
+const getFirstCycleNode = (cycleNode, headNode, size) => {
+	let count = 0;
+	let current = headNode;
+	while (current != cycleNode) {
+		current = current.next;
+		count += 1;
+	}
+	console.log(count);
+	let moveCounter = Math.abs(size - (count - size));
+	let currentCycle = cycleNode;
+	while (moveCounter !== 0) {
+		currentCycle = currentCycle.next;
+		moveCounter -= 1;
+	}
+	return currentCycle;
+};
+
 // determine if there is a cycle
 // return length of cycle
 // return first node in cycle
@@ -76,13 +105,20 @@ const cycleInformation = (head) => {
 // determine if there is a cycle
 // get first node in the cycle
   let currentNode = head;
+  // return the cycle node if there is a cycle;
   let cycle = isThereCycle(currentNode);
   if (!cycle) throw "no cycle @ all!";
-  let sizeOfCycle = findCycleLength(currentNode);
-  return [sizeOfCycle, getFirstCycleNode(currentNode), ];
+  let sizeOfCycle = findCycleLength(cycle);
+  return [sizeOfCycle, getFirstCycleNode(cycle, sizeOfCycle)];
 }
 
-let head = makeCyclicLinkedList([1,2,3,4,5,6,7,8,9,10,11,12])
-console.log(head.data);
-console.log(head.next);
-console.log(head.next.next.next.next.next.next.next.next.next.next)
+let head = makeCyclicLinkedList([1,2,3,4,5,6,7,8,9,10,11,12]);
+let noCyclicHead = makeNodeLinkedList([1,2,3,4,5,6,7,8,9,10,11,12]);
+console.log(noCyclicHead.next.next.next.next.next.next.next.next.next.next)
+// console.log(head.data);
+// console.log(head.next);
+// console.log(head.next.next.next.next.next.next.next.next.next.next)
+// console.log(isThereCycle(noCyclicHead));
+// console.log(isThereCycle(head));
+// console.log(isThereCycle(head))
+console.log(cycleInformation(head));
