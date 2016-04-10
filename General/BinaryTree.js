1f8d0c9532210affe75e905abd4169cfcd8ad36e
Victor Hom <vh62@cornell.edu>
2016-04-09 22:16:26 -0400

    added comments about tree

---
+++ General/BinaryTree.js
@@ -0,0 +1,45 @@
+// Notes on Binary Tree
+// a tree is a type of graph
+// A binary tree is different from a binary search tree
+// in a bst, each node fits the condition where all left descendants
+// are less than the node and all right descendants are greater
+
+// balance v. unbalance
+// balance means good enough to ensure O(lg n) for insert and find
+
+// complete binary trees
+// every level of the tree is fully filled, except the last level
+// if that is the case, it is filled left to right
+// if the last filled is right, it is not complete
+
+// full binary trees
+// every node has zero or 2 children
+
+// perfect binary trees
+// both full and complete
+// all leaf nodes will be at the same level, and this level
+// will have max number of nodes
+
+// BST traversals
+// In-Order Traversals - vist left branch, current node, then right
+// Pre-Order Traversals - visit the current node, left node, right nodes
+// Post-Order Traversals - visit left node, visit right node, visit current nodes
+
+// Binary Heaps (Min Heaps and Max Heaps)
+// a min heap is a complete binary tree
+// in a min heap where each node is smaller than its children
+// the root is therefore is the min element in the tree
+// min heap has two key operations : insert and extract-min
+
+// insert - we always start by inserting
+// the elements at the bottom to maintain complete property
+// then we fix the tree by swapping the new element with its parent,
+// until we find appropriate spot for the elements
+// insert should be O(lg n) time
+
+// extract is easy
+// it's always at the top
+// the tricky part is how to remove it
+// swap it with the last element in the heap (bot rightmost element)
+// then swap it childrent until properties is restored
+// also O(lg n)
