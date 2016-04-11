94891b967fadbb800009e7a89141b5b7a5fa0bbc
Victor Hom <vh62@cornell.edu>
2016-04-11 18:56:06 -0400

    added another written version of mergesort

--- Sorts/merge.js
+++ Sorts/merge.js
@@ -1,5 +1,5 @@
 // merge sort has  an average and worst case of O(n log n)
-
+// O(n) for space complexity
 const split = (list) => {
 	let midPoint = Math.floor(list.length / 2);
 	let leftArray = list.slice(0 , midPoint);
@@ -41,3 +41,49 @@ export const mergeSort = (list) => {
 // merge sort is made up of a merge piece and a sort piece
 // I think the version below looks better for reading
 //https://github.com/nzakas/computer-science-in-javascript/blob/master/algorithms/sorting/merge-sort-recursive/merge-sort-recursive.js
+
+
+// writing merge sorting cold
+// on 4/11/2016
+
+const sort = (arr) => {
+	if(arr.length <= 2) {
+		if (arr.length <= 1) {
+			return arr;
+		} else if (arr.length === 2) {
+			let val1 = arr[0];
+			let val2 = arr[1];
+			if (val1 < val2) return arr;
+			return [val2, val1];
+		}
+	}
+	return mergeSort(arr);
+}
+
+const merge = (arrA, arrB) => {
+	let indexA = 0;
+	let indexB = 0;
+	let finalArr = [];
+	while (indexA < arrA.length && indexB < arrB.length) {
+		if (arrA[indexA] < arrB[indexB]) {
+			finalArr.push(arrA[indexA]);
+			indexA += 1;
+		} else {
+			finalArr.push(arrB[indexB]);
+			indexB += 1
+		}
+	}
+
+	if (indexA === arrA.length) return finalArr.concat(arrB.slice(indexB));
+	if (indexB === arrB.length) return finalArr.concat(arrA.slice(indexA));
+}
+
+
+const mergeSort = (arr) => {
+	if (arr.length <= 1) return arr;
+	let midIndex = Math.floor(arr.length / 2);
+	let lowerHalf = sort(arr.slice(0, midIndex));
+	let upperHalf = sort(arr.slice(midIndex));
+	return  merge(lowerHalf, upperHalf);
+}
+////// end
