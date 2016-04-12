9bd01146b2b8c0ca139d388336d42b9f560dad20
Victor Hom <vh62@cornell.edu>
2016-04-11 20:19:27 -0400

    added a version of a cycle loop function

--- General/cycleLoops.js
+++ General/cycleLoops.js
@@ -1,20 +1,30 @@
+const nextLocation = (arrLength, jump, current) => {
+	let pt = 0;
+	if (jump + current > arrLength-1) {
+	    pt = (jump + current) % arrLength;
+	} else if (jump + current < 0) {
+	  	let val = ((Math.abs(jump + current)) % arrLength);
+	    pt = (arrLength) - val;
+	} else {
+		pt = jump + current;
+	}
+	return pt;
+}
 const cycleLoops = (arr) => {
   let marker = [];
-  marker[0] = true //visited
-  let jump = arr[0];
-  let current = 0;
-
-  let pt = 0;
-  // need to check
-  if (jump + current > arr.length) {
-    pt = (jump + current) % arr.length;
-  } else  {
-    pt = (arr.length-1) - ((Math.abs(jump + current)) % arr.length);
-  }
-  // need to do this, but it should not be bad
+	let jump = arr[0];
+	let current = 0;
+	let pt = current;
   while (!marker[pt]) {
-
+  	marker[pt] = true;
+	pt = nextLocation(arr.length, jump, current)
+	jump = arr[pt];
+	current = pt;
   }
   if (marker.length === arr.length) return true
   return false;
 }
+
+cycleLoops([1,1,-2,1])
+cycleLoops([2,2,-1,1])
+cycleLoops([2,3,-1,1])
