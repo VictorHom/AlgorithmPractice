c0c754f02b4de39ca3bfb898750ff827f0ecec1f
Victor Hom <vh62@cornell.edu>
2016-04-11 21:41:42 -0400

    working on dijkstra

---
+++ General/dijkstra.js
@@ -0,0 +1,41 @@
+// algo for finding shortest path between nodes in a graph
+// such as road networks
+// find the shortest path from src to destination
+
+
+class Node {
+  constructor(distance, name) {
+    this.name = name;
+    this.distance;
+    this.neighbors = [];
+    this.visited = false;
+  }
+}
+
+// to be used between consecutive test cases
+// that will use the same nodes
+const clearVisits = (arrOfNodes) => {
+  arrOfNodes.forEach(elem => {
+    elem.visited = false;
+  })
+};
+
+// dijkstra is a breadth first search
+const dijkstraSearch = (src, dest) => {
+  // using the array as a queue, so will shift and push on
+  let queue = [];
+  let srcNeighbors = src.neighbors;
+  queue = queue.concat(srcNeighbors);
+  while (queue.length !== 0) {
+    
+  }
+}
+// dijkstra's algorithm
+// used to calculate the shortest path from src to destination
+// set the initial node as the currentNode
+// mark all other nodes as unvisited
+// create a set of all the unvisited nodes called the unvisited set
+// for the current node, consider all of its unvisited neighbors and calculate
+// their tentative distances
+// compare the newly calculated tentative distance to the current assigned value and assign the smaller one
+// if the value is greater, then change it, otherwise keep.
