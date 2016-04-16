eae2449f4d339389ace6b78b22d3f4a708d21edb
Victor Hom <vh62@cornell.edu>
2016-04-16 17:47:17 -0400

    added a o(n) soln to the getmaxprofit problem

--- General/getMaxProfits.js
+++ General/getMaxProfits.js
@@ -7,22 +7,56 @@
 
 // where is the greatest profit located if you buy one sell
 
-var getMaxProfit = arr => {
-    let min = arr[0];
-    let maxProf = 0;
-    arr.forEach(num => {
-        if (num < min) min = num;
-        if (maxProf < num - min) maxProf = num - min;
-    })
-    return maxProf;
+const getMaxProfit = (stockPrices) => {
+	let profits = 0;
+
+	let i = 0, j = stockPrices.length-1;
+	while (i != j) {
+		let buyOne = stockPrices[i];
+		let sellOne = stockPrices[j];
+		if ((sellOne - buyOne) > 0) {
+			if (profits < (sellOne - buyOne)) {
+				profits = sellOne - buyOne;
+			}
+			i++;
+		} else {
+			j--;
+		}
+	}
+
+	return profits;
 }
-//https://repl.it/CAsT/2
-var stockPricesYesterday = [10, 7, 5, 8, 11, 9];
-var anotherTest = [100, 180, 260, 310, 40, 535, 695];
-var small = [100, 180, 260];
-console.log(getMaxProfit(stockPricesYesterday));
-console.log(getMaxProfit(anotherTest))
-console.log(getMaxProfit(small));
+
+// a recursive solution
+const getMaxProfit = (prices) => {
+	// base cases
+	if (prices.length === 2) {
+		if (prices[1] - prices[0] > 0) {
+			return prices[1] - prices[0];
+		}
+		return -1;
+	} else {
+		// make the cases to become base cases
+		// if the first price is the lowest price
+		let firstCase = Math.max.apply(null, prices.slice(1)) - prices[0];
+		// where the first price is not the lowest price
+		let secondCase = getMaxProfit(prices.slice(1))
+		return Math.max.apply(null, [firstCase].concat([secondCase]));
+	}
+}
+
+const getMaxProfit = (prices) => {
+	let maxProfit = 0;
+	let minPrice = prices[0];
+
+	for (let i = 0; i < prices.length; i++) {
+		let currentMinPrice = Math.min(minPrice, prices[i]);
+		minPrice = currentMinPrice;
+		let currentProfit = prices[i] - currentMinPrice;
+		maxProfit = Math.max(currentProfit, maxProfit);
+	}
+	return maxProfit;
+}
 
 // NEXT Step is to see if you can find the max profit
 // multiple times of buying and selling in a given time period
