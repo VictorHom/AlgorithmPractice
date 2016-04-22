f76e502d0442b3c93d144afed729365f7d8e2d62
Victor Hom <vh62@cornell.edu>
2016-04-21 20:56:12 -0400

    added a recursive method for checking if a bst is balanced

--- General/balancedBST.js
+++ General/balancedBST.js
@@ -6,25 +6,39 @@ class Node {
 	}
 }
 
-// is the tree balanced?
+let A = new Node("A");
+let B = new Node("B");
+let C = new Node("C");
+let D = new Node("D");
+let E = new Node("E");
+let F = new Node("F");
+let G = new Node("G");
 
-const BalancedTree_BFS = (head) => {
-	let multiplier = 2;
-
-}
+A.right = B;
+A.left = C;
+B.right = D;
+C.right = E;
+B.left = F;
+C.left = G;
 
 const BalancedTree_DFS = (head) => {
-	if (!head.left && !head.right) {
-		return 0;
-	} else {
-		let rightHeight = 0;
-		let leftHeight = 0;
-		if (head.right) {
-			rightHeight =  1 + BalancedTree_DFS(head.right)
+
+	const getHeightDifference = (node) => {
+		if (!head.left && !head.right) {
+			return 0;
+		} else {
+			let rightHeight = 0;
+			let leftHeight = 0;
+			if (head.right) {
+				rightHeight =  1 + BalancedTree_DFS(head.right)
+			}
+			if (head.left) {
+				leftHeight = 1 + BalancedTree_DFS(head.left);
+			}
+			return Math.abs(rightHeight - leftHeight);
 		}
-		if (head.left) {
-			leftHeight = 1 + BalancedTee_DFS(head.left);
-		}
-		return Math.abs(rightHeight - leftHeight) <= 1;
 	}
+	return getHeightDifference(head) <= 1;
 }
+
+console.log(BalancedTree_DFS(A));
